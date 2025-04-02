dfd34309e258c5684ceb8ef0fd93f554ea671f3d
softpartner2023 <abdellatif2@hotmail.com>
2025-04-02 22:33:05 +0200

    Init project

---
+++ src/main/java/com/cacib/msgconsumer/MsgconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.cacib.msgconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cacib/msgconsumer/MsgconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacib.msgconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
